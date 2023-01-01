b892ba0d0654d84938bf28ee704cb06ed46a4512
ben <46631004+cyben-x@users.noreply.github.com>
2022-12-31 17:18:57 -0700

    scenehandler

---
+++ unity project/MelonJam_project/Assets/sceneHandler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneHandler : MonoBehaviour
+{
+    float scene1Timer = 10;
+    float elapsedTime = 0;
+    public Scene intro;
+    public Scene game;
+    public Scene ending;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("IntroCutscene");
+        intro = SceneManager.GetSceneByName("IntroCutscene");
+        game = SceneManager.GetSceneByName("SampleScene");
+        ending = SceneManager.GetSceneByName("EndingCutscene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (SceneManager.GetActiveScene() == intro)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime > scene1Timer)
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+        else if (SceneManager.GetActiveScene() == game)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player.GetComponent<ClothingHandler>().hasHat &&
+                player.GetComponent<ClothingHandler>().hasScarf &&
+                player.GetComponent<ClothingHandler>().hasPipe)
+            {
+                SceneManager.LoadScene("EndingCutscene");
+            }
+        }
+    }
+}
